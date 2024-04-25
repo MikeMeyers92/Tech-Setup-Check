23b82fc6254aaf42124ab01d0d84bb426a0805e4
mike <mikemeyersmc@gmail.com>
2024-04-24 22:10:46 -0700

    first commit

---
+++ notes.js
@@ -0,0 +1,51 @@
+//console.log("what's good!");
+
+// let counter = 3
+
+// console.log("Counter before the loop", counter);
+
+// for (let i = 0; i < 10; i++ ) {
+//     console.log("----------------------------A new loop just began!----------------------------------------")
+// console.log('Counter before the math', counter);
+
+//     counter = (i + 6) * counter;
+
+//     console.log('Counter after the math', counter);
+// }
+
+//!------------------short circuiting--------------------------
+
+// console.log('Counter after the loop', counter);
+
+// let theNumIsEven = 11;
+
+// console.log(theNumIsEven % 2);
+
+// if (theNumIsEven % 2 === 0) {
+//     console.log('Yes this is even!');
+// } else {
+//     console.log('No it is odd!');
+// }
+
+//!------------------boolean--------------------------
+
+// let checkBoolVal = "false"; // strings are always true
+
+// if (checkBoolVal) {
+//     console.log('This value is truuueee');
+// } else {
+//     console.log('Nuh uhhhh not true, false');
+// }
+
+//!------------------short circuiting--------------------------
+
+let checkBoolVal = "false"; // strings are always true
+
+if (jfdklsafphdsaj) {
+    console.log('This value is truuueee');
+} else {
+    console.log('Nuh uhhhh not true, false');
+}
+
+
+//!--------------------------------------------
